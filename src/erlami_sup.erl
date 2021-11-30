16422b17befe7b4591d8ac0902ba9f5d37fc5f15
root <root@conductor.onnet.su>
2021-11-30 19:34:31 +0000

    update

--- src/erlami_sup.erl
+++ src/erlami_sup.erl
@@ -21,7 +21,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, start_child/2]).
+-export([start_link/0, start_child/2, stop/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -43,6 +43,18 @@ start_link() ->
 
 start_child(ServerName, ServerInfo) ->
     supervisor:start_child(?MODULE, [ServerName, ServerInfo]).
+
+-spec stop(pid()|string()|atom()) -> 'ok' | {'error', 'not_found'}.
+stop(Pid) when is_pid(Pid) ->
+    supervisor:terminate_child(?MODULE, Pid);
+stop(ServerName) ->
+    case whereis(erlami_client:get_worker_name(ServerName)) of
+        Pid when is_pid(Pid) ->
+            supervisor:terminate_child(?MODULE, Pid);
+        _ ->
+            {'error', 'not_found'}
+    end.
+
 
 %% ===================================================================
 %% Supervisor callbacks
